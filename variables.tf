d3567366ad58aca5f88e3ba7da3cb9dd52a58e0a
Abhishek Chaudhary <chaudha4@hotmail.com>
2021-08-20 18:34:09 -0400

    Add defaults

--- variables.tf
+++ variables.tf
@@ -1,7 +1,9 @@
 variable "prefix" {
   description = "The prefix used for all resources in this example"
+  default = "prefix1"
 }
 
 variable "location" {
   description = "The Azure location where all resources in this example should be created"
+  default = "East US 2"
 }
